1f6069d6db6303dd1296386cb9b7659ca322c52d
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-08-09 01:05:28 +0900

    08.09

---
+++ SampleScrolView/app/src/main/java/com/example/samplescrolview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.samplescrolview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+    ImageView imageView;
+    BitmapDrawable bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        scrollView = findViewById(R.id.scrollView);
+        imageView = findViewById(R.id.imageView);
+        scrollView.setHorizontalScrollBarEnabled(true);
+
+    }
+}
