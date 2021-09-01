80a4212e978a314a4c74b4d49db4495a4516d6e2
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-09-01 22:27:48 +0900

    editext

--- SampleSpinnerMakeText/app/src/main/java/org/techtown/samplespinnermaketext/MainActivity.java
+++ SampleSpinnerMakeText/app/src/main/java/org/techtown/samplespinnermaketext/MainActivity.java
@@ -3,48 +3,74 @@ package org.techtown.samplespinnermaketext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
 
-        TextView textView;
+    TextView textView;
 
-        //데이터를 배열에 넣어서 준비
-        String[] items = {"fish", "turtle", "seal"};
+    //데이터를 배열에 넣어서 준비
+    String[] items = {"fish", "turtle", "seal"};
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    //EditText 설정
+    EditText extTest = (EditText) findViewById(R.id.ext_test);
 
-            Spinner spinner = findViewById(R.id.spinner);
-            textView = findViewById(R.id.textView);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    //API에 만들어져 있는 R.layout.simple_spinner...를 씀
-                    this,android.R.layout.simple_spinner_item, items
-            );
-            //미리 정의된 레이아웃 사용
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-            // 스피너 객체에다가 어댑터를 넣어줌
-            spinner.setAdapter(adapter);
-            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                // 선택되면
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    textView.setText(items[position]);
-                }
+        Spinner spinner = findViewById(R.id.spinner);
+        textView = findViewById(R.id.textView);
 
-                // 아무것도 선택되지 않은 상태일 때
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
-                    textView.setText("선택: ");
-                }
-            });
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                //API에 만들어져 있는 R.layout.simple_spinner...를 씀
+                this, android.R.layout.simple_spinner_item, items
+        );
+        //미리 정의된 레이아웃 사용
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        // 스피너 객체에다가 어댑터를 넣어줌
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            // 선택되면
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(items[position]);
+            }
 
-        }
+            // 아무것도 선택되지 않은 상태일 때
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                textView.setText("선택: ");
+            }
+        });
+        //EditText Listener 설정
+        extTest.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //텍스트 입력이 모두 끝았을때 Call back
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //텍스트가 변경 될때마다 Call back
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //텍스트가 입력하기 전에 Call back
+            }
+        });
+
+    }
+
+
+
 }
