debd6b1b47019c31bebf5efa55f292a13c24413b
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-10-04 20:56:26 +0900

    10.04

--- SingleDiary/app/src/main/java/org/techtown/diary/Fragment3.java
+++ SingleDiary/app/src/main/java/org/techtown/diary/Fragment3.java
@@ -7,11 +7,16 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+
 public class Fragment3 extends Fragment {
 
-   
+    PieChart chart;
+    BarChart chart2;
+    LineChart chart3;
+
+
     @Override
-                       
+
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
                                         // LayoutInflater 객체의 inflate 메서드 호출
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment3,container,false);
@@ -23,6 +28,9 @@ public class Fragment3 extends Fragment {
 
     // 인플레이션 후 xml 레이아웃 안에 들어있는 위젯 OR 레이아웃을 찾아 변수에 할당하는 코드들을 넣기 위해 만들어 둔것
     private void initUI(ViewGroup rootView){
-        
+
+/*        chart = rootView.findViewById(R.id.chart1);
+        chart.setUsePercentValues(true);
+        chart.getDescription().setEnabled(false);*/
     }
 }
